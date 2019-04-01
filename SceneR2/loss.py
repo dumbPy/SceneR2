3c21134a8a9eb4c74227e9396a1d161b40069468
Sufiyan Adhikari <dumbpyx@gmail.com>
2019-04-01 15:19:57 +0530

    focalMSE fixed

--- SceneR2/loss.py
+++ SceneR2/loss.py
@@ -8,15 +8,15 @@ class weightedMSE(torch.nn.MSELoss):
         self.weight=torch.from_numpy(np.asarray([weight])).float().to(device)
 
         super().__init__(*args, **kwargs)
-    def forward(self, input, target):
-        return (((input-target)**2)*self.weight.expand(target.shape)).mean()
+    def forward(self, pred, target):
+        return (((pred-target)**2)*self.weight.expand(target.shape)).mean()
 
 class FocalMSE(torch.nn.MSELoss):
     def __init__(self,threshold=0.7, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.threshold=threshold
 
-    def forward(self, input, target):
-        delta = input-target
-        return (((delta)**2)*(delta<self.threshold).float()).mean()
+    def forward(self, pred, target):
+        delta = pred-target
+        return (((delta)**2)*(pred<self.threshold).float()).mean()
 
