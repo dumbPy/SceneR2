9b67b0cec9d29122885c89469b5d882d359c8f85
Sufiyan Adhikari <dumbpyx@gmail.com>
2019-06-03 17:04:19 +0530

    view.py with model.save and debug code

--- SceneR2WebApp/webapp/views.py
+++ SceneR2WebApp/webapp/views.py
@@ -1,38 +1,50 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib import messages
-from .forms import UploadCANForm, UploadVideoForm
+from django.conf import settings
+from .forms import UploadVideoAndCANForm
 import os
 import pkg_resources
 from .process import process_can_and_video
 from django.templatetags.static import static
+from PIL import Image
 
 # def index(request):
 #     return render(request, 'index.html')
 
 def index(request):
     if request.method == 'POST':
-        video_form = UploadVideoForm(request.POST, request.FILES)
-        can_form   = UploadCANForm(  request.POST, request.FILES)
-        if video_form.is_valid() and can_form.is_valid():
-            for key in request.FILES:
-                file = request.FILES[key]
-                os.makedirs('tmp', exist_ok=True)
-                path = static(f'data/{file.name}')
-                ext = file.name.split('.')[-1]
-                if ext in ['mp4', 'avi']: vid_filename = file.name
-                if ext == 'csv': can_filename = file.name
-                with open(path, 'wb+') as f:
-                    for chunk in file.chunks():
-                        f.write(chunk)
-            try: pred = process_can_and_video(static('data/'), 
-                        can_filename, vid_filename)
-            except:
-                # Wrong file types uploaded
-                messages.error(request, 'Invalid File types. upload CAN csv and corresponding video (mp4 or avi)')
-                form = UploadFileForm()
-                return render(request, 'index.html', {'form':form})
-            return render(request, 'output.html', {'form':form})
+        form = UploadVideoAndCANForm(request.POST, request.FILES)
+        
+        if form.is_valid():
+            form = form.save()
+            out_folder = os.path.join(settings.MEDIA_ROOT, 'processed')
+            message, params = process_can_and_video(out_folder, 
+                        form.can.path, form.video.path)
+            vid_filename = form.video.name.split('/')[-1]
+            form = UploadVideoAndCANForm()
+            messages.info(request, message)
+            context = {'form':form,
+                    'video_path':f'/media/processed/{vid_filename}',
+                    'can_slider_path': f'/media/processed/can_slider.png',
+                    'can_image_full': f'/media/processed/can_image_full.png',
+                    'can_few_cols': f'/media/processed/can_few_cols.png'}
+            context.update(params)
+            return  render(request, 'output.html', context)
+        else:
+            form = UploadVideoAndCANForm()
+            messages.error(request, 'Invalid Uploads extensions, video should be either mp4 or avi and CAN should be a csv files')
+            return render(request, 'index.html', {'form':form})
     else:
-        form = UploadFileForm()
-    return render(request, 'index.html', {'form':form})
+        form = UploadVideoAndCANForm()
+    out_folder = os.path.join(settings.MEDIA_ROOT, 'processed')
+    slider_size = Image.open(os.path.join(out_folder, 'can_slider.png')).size
+    w,h = slider_size
+    slider_height = 0.9*h
+    return render(request, 'output.html', {'form':form,
+                    'video_path':'/media/processed/20170213_042040_Video_MJloQEH.mp4',
+                    'can_slider_path': f'/media/processed/can_slider.png',
+                    'can_image_full': f'/media/processed/can_image_full.png',
+                    'can_few_cols': f'/media/processed/can_few_cols.png','len_can':996,
+                    'slider_height': f'{slider_height}px',
+                    'len_video':498})
