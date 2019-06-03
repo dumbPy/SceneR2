f052469e190468e48ba7bb68c8e936b45eab64fb
Sufiyan Adhikari <dumbpyx@gmail.com>
2019-06-03 17:02:23 +0530

    adding media and static to urls

--- SceneR2WebApp/SceneR2WebApp/settings.py
+++ SceneR2WebApp/SceneR2WebApp/settings.py
@@ -119,4 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 MEDIA_ROOT = '/home/sufiyan/Dropbox/MTP/Code_and_Notebook/SceneR2WebApp/webapp/media'
+MEDIA_URL = '/media/'
--- SceneR2WebApp/SceneR2WebApp/urls.py
+++ SceneR2WebApp/SceneR2WebApp/urls.py
@@ -21,4 +21,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', include('webapp.urls')),
     path('admin/', admin.site.urls),
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]   +static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)\
+    +static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
