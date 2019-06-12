22f45c3a836b2df9bd3437fde1a2a41f2bf5a8c5
Sufiyan Adhikari <dumbpyx@gmail.com>
2019-06-12 18:55:43 +0530

    webapp now shows running time too

--- SceneR2WebApp/webapp/static/js/slider.js
+++ SceneR2WebApp/webapp/static/js/slider.js
@@ -2,6 +2,9 @@ var slider = document.getElementById('myRange');
 var video = document.getElementById('video1');
 var fps_slider = document.getElementById('fps_slider');
 var print_fps = document.getElementById('print_fps');
+var p_ct = document.getElementById('print_currentTime');
+
+video.ontimeupdate = function(){p_ct.innerHTML = (video.currentTime).toFixed(1)+'/'+video.duration}
 
 function setSliderValue(video){
   if (video.paused || video.ended) return false;
